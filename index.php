2d2541df82f91f5873f36d030aeb81203898c96d
nela6246 <nnela201398@bscse.uiu.ac.bd>
2024-07-03 12:04:55 +0600

    add task 1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CT 1 </title>
+</head>
+<body>
+    <h1>CT 1</h1>
+    <ul>
+        <li><a href="read_rickshaw.php">Read Rickshaws with Speed > 10</a></li>
+        <li><a href="read_driver.php">Read Drivers Joined After June 24, 2023</a></li>
+        <li><a href="update_speed.php">Update Rickshaw Speed to 15 Where Speed < 5</a></li>
+    </ul>
+</body>
+</html>
